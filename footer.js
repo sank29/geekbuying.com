c7e9e9779e9a498a09361936ff22672fceedd978
sanket <wankhdes29@gmail.com>
2022-07-21 16:41:36 +0530

    footer almost done some css part remaining

--- footer.js
+++ footer.js
@@ -1,5 +1,87 @@
 let footerFirstPartDiv = document.querySelector("#footerFirstPart");
+let newDivFooter = document.createElement("div");
+let footerPlus = document.querySelector("#footerPlus");
 
 footerFirstPartDiv.addEventListener("click", (event) => {
-  console.log("hiiii");
+  newDivFooter.setAttribute("id", "footerDropDown");
+
+  let newDivFooterArr = document.querySelectorAll("#footerDropDown");
+  console.log(newDivFooter.style.display);
+
+  // if (newDivFooterArr.length === 1) {
+  //   newDivFooter.style.display === "none"
+  //     ? (newDivFooter.style.display = "flex")
+  //     : (newDivFooter.style.display = "none");
+  //   return false;
+  // }
+
+  if (newDivFooterArr.length === 1) {
+    if (newDivFooter.style.display === "none") {
+      newDivFooter.style.display = "flex";
+      footerPlus.innerText = "-";
+    } else {
+      newDivFooter.style.display = "none";
+      footerPlus.innerText = "+";
+    }
+    return false;
+  }
+  footerPlus.innerText = "-";
+
+  let newSubDivfirst = document.createElement("div");
+  let img1 = document.createElement("img");
+  img1.src = "./img/percentage.png";
+
+  let p1 = document.createElement("p");
+  p1.innerText = "Amazing offers";
+
+  let p2 = document.createElement("p");
+  p2.innerText = "Find best offers for smart gadgets at one place.";
+
+  newSubDivfirst.append(img1, p1, p2);
+
+  let newSubDivSecond = document.createElement("div");
+  let img2 = document.createElement("img");
+  img2.src = "./img/wallet.png";
+
+  let p3 = document.createElement("p");
+  p3.innerText = "Easy & secure payments";
+
+  let p4 = document.createElement("p");
+  p4.innerText =
+    "Pay directly via PayPal or Klarna among other payment options.";
+
+  newSubDivSecond.append(img2, p3, p4);
+
+  let newSubDivThird = document.createElement("div");
+  let img3 = document.createElement("img");
+  img3.src = "./img/truck.png";
+
+  let p5 = document.createElement("p");
+  p5.innerText = "Local stock & fast shipping";
+
+  let p6 = document.createElement("p");
+  p6.innerText = "Buy from local stock and ship within 48-hour.";
+
+  newSubDivThird.append(img3, p5, p6);
+
+  let newSubDivFourth = document.createElement("div");
+  let img4 = document.createElement("img");
+  img4.src = "./img/customer-support.png";
+
+  let p7 = document.createElement("p");
+  p7.innerText = "Customer support";
+
+  let p8 = document.createElement("p");
+  p8.innerText = "24-hour customer support for all your queries.";
+
+  newSubDivFourth.append(img4, p7, p8);
+
+  newDivFooter.append(
+    newSubDivfirst,
+    newSubDivSecond,
+    newSubDivThird,
+    newSubDivFourth
+  );
+
+  footerFirstPartDiv.after(newDivFooter);
 });
